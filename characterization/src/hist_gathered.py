fdc33051e5b5932dcc54651fd5fd91ece1b23dbe
Manuela Kuhn <manuela.kuhn@desy.de>
2018-03-19 22:46:29 +0100

    Added first version of plotting tools

---
+++ characterization/src/hist_gathered.py
@@ -0,0 +1,30 @@
+import glob
+import h5py
+import matplotlib
+matplotlib.use('Agg')  # Must be before importing matplotlib.pyplot or pylab!
+import matplotlib.pyplot as plt  # noqa E402
+import numpy as np
+import os
+
+from plot_base import PlotBase
+
+class HistGathered(PlotBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def _generate_single_plot(self, x, data, plot_title, label, out_fname, nbins):
+
+        data = np.hstack(data)
+
+        # plot data
+        fig = plt.figure(figsize=None)
+
+        plt.hist(data, bins=nbins)
+        plt.xlabel('ADU');
+        plt.ylabel('#values');
+
+        fig.suptitle(plot_title)
+        fig.savefig(out_fname)
+
+        fig.clf()
+        plt.close(fig)
