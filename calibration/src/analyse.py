c954470f769034a43ef279a3eaaf5c9cfbd06da0
benjaminboitrelle <benjamin.boitrelle@desy.de>
2018-03-22 17:13:01 +0100

    Add parser arguments

--- calibration/src/analyse.py
+++ calibration/src/analyse.py
@@ -7,6 +7,7 @@ import time
 import glob
 
 import utils
+import argparse
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 SRC_PATH = os.path.join(BASE_PATH, "src")
@@ -160,20 +161,62 @@ class Analyse(object):
 
 if __name__ == "__main__":
 
-    run_id = "DLSraw"
-    in_base_dir = "/gpfs/cfel/fsds/labs/agipd/calibration/scratch/user/kuhnm/percival_tests/P2M_ADCcor_crs_reduced"
+    parser = argparse.ArgumentParser(description="Calibration tools for P2M")
+    parser.add_argument("-i", "--input", dest = "in_base_dir", type = str,
+                        help = "Path of input directory containing HDF5 files to analyse")
+    parser.add_argument("-o", "--output", dest = "out_base_dir", type = str,
+                        help = "Path of output directory for storing files")
+    parser.add_argument("-r", "--run", dest = "run_id", type = str,
+                        help = "Run id")
+    parser.add_argument("-m", "--method", dest = "method", type = str,
+                        help = "Method to use during the analysis: "  
+                               "process_adccal_default, "
+                               "None")
+    parser.add_argument("-t", "--type", dest = "run_type", type = str,
+                        help = "Run type: "
+                               "gather, " 
+                               "process")
+    args = parser.parse_args()
+
+    run_id = args.run_id
+    run_type = args.run_type
+    method = args.method
     n_cols = None
+    meas_type = "adccal"
+    if run_type == "gather":
+        in_base_dir = args.in_base_dir
+        out_base_dir = args.out_base_dir + "{}_gathered".format(run_id)
+    else:
+        in_base_dir = args.in_base_dir + "{}_gathered".format(run_id)
+        out_base_dir = args.out_base_dir + "{}_processed".format(run_id)
+    
+    obj = Analyse(in_base_dir,
+                  out_base_dir,
+                  run_id,
+                  run_type,
+                  meas_type,
+                  n_cols,
+                  method)
+    obj.run()
+     
+    #in_base_dir = args.in_base_dir + "{}_gathered".format(run_id)
+    #out_base_dir = args.out_base_dir +"{}_processed".format(run_id)
+    #method = args.method
+
+   # run_id = "DLSraw"
+   #in_base_dir = "/gpfs/cfel/fsds/labs/agipd/calibration/scratch/user/kuhnm/percival_tests/P2M_ADCcor_crs_reduced"
+    #n_cols = None
 
 #    in_base_dir = "/nfs/fs/fsds/percival/P2MemulatedData/ADCcorrection/58_W08_01_TS1.2PIX_PB5V2_-40_N02_25MHz_1ofmany_all_coldFingerT-40/P2Mdata_coldFingerT-40"
 #    run_id = "raw_uint16"
 #    n_cols = 64
 
-    g_out_base_dir = "/gpfs/cfel/fsds/labs/agipd/calibration/scratch/user/kuhnm/percival_tests/{}_gathered".format(run_id)
-    p_out_base_dir = "/gpfs/cfel/fsds/labs/agipd/calibration/scratch/user/kuhnm/percival_tests/{}_processed".format(run_id)
+    #g_out_base_dir = "/gpfs/cfel/fsds/labs/agipd/calibration/scratch/user/kuhnm/percival_tests/{}_gathered".format(run_id)
+    #p_out_base_dir = "/gpfs/cfel/fsds/labs/agipd/calibration/scratch/user/kuhnm/percival_tests/{}_processed".format(run_id)
 
-    run_type = "gather"
-    meas_type = "adccal"
-    method = None
+   # run_type = "gather"
+   # meas_type = "adccal"
+   # method = None
 
     # run gather
 #    g_obj = Analyse(in_base_dir,
@@ -187,16 +230,17 @@ if __name__ == "__main__":
 
 #    del g_obj
 
-    run_type = "process"
-    meas_type = "adccal"
-    method = "process_adccal_default"
+ #   run_type = "process"
+ #   meas_type = "adccal"
+    #method = "process_adccal_default"
 
     # run process
-    p_obj = Analyse(g_out_base_dir,
-                    p_out_base_dir,
-                    run_id,
-                    run_type,
-                    meas_type,
-                    n_cols,
-                    method)
-    p_obj.run()
+#    p_obj = Analyse(in_base_dir,
+#                    out_base_dir,
+#                    run_id,
+#                    run_type,
+#                    meas_type,
+#                    n_cols,
+#                    method)
+#    p_obj.run()
+
