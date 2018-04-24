a673dcf859dd25149dce7efbcfedba7fdb408967
benjaminboitrelle <benjamin.boitrelle@desy.de>
2018-04-24 15:36:08 +0200

    Fitting method with range (static)

--- calibration/src/process/adccal/methods/process_adccal_default.py
+++ calibration/src/process/adccal/methods/process_adccal_default.py
@@ -8,7 +8,7 @@ class Process(ProcessAdccalBase):
 
     def _initiate(self):
         shapes = {
-            "offset": (self._n_cols, self._n_adcs)
+            "offset": (self._n_adcs, self._n_cols)
         }
 
         self._result = {
@@ -16,12 +16,11 @@ class Process(ProcessAdccalBase):
             "s_coarse_offset": {
                 "data": np.zeros(shapes["offset"]),
                 "path": "sample/coarse/offset",
-                "type": np.int16
+                "type": np.float16
             },
             "s_coarse_slope":{
                 "data": np.zeros(shapes["offset"]),
-                "path": "sample/coarse/slope",
-                "type": np.int16
+                "path": "sample/coarse/slope"
             },
 
             # additional information
@@ -44,87 +43,32 @@ class Process(ProcessAdccalBase):
         data = self._load_data(self._in_fname)
         print("Done.")
 
-        #print(data["s_coarse"][self._n_adcs - 1, self._n_cols - 1, 0, :])
-       
         # convert (n_adcs, n_cols, n_groups, n_frames)
         #      -> (n_adcs, n_cols, n_groups * n_frames)
         
         sample_coarse = self._merge_groups_with_frames(data["s_coarse"])
 
-        #print(sample_coarse[self._n_adcs - 1, self._n_cols - 1, :]) # All values corresponding to one ADC of one column
-        #print(sample_coarse[1, 100, :]) # All values corresponding to one ADC of one column
-
         # create as many entries for each vin as there were original frames
-<<<<<<< HEAD
         vin = self._fill_up_vin(data["vin"])
+
         slope = self._result["s_coarse_slope"]["data"]
         offset = self._result["s_coarse_offset"]["data"]
 
         fit_result_list = []
         for adc in range(self._n_adcs):
-            #print(adc)
-            for col in range(33, self._n_cols):
-                adu_coarse = sample_coarse[adc, col, :]
-                fit_result = self._fit_linear(vin, adu_coarse)
-                slope[adc, col] = fit_result[0]
-                offset[adc, col] = fit_result[1]
-                self._result["s_coarse_slope"]["data"] = slope
-                self._result["s_coarse_offset"]["data"] = offset
-
-
-
-                #fit_result = self._fit_linear(vin, adu_coarse)
-                #fit_result_list.append(fit_result[0])
-                #fit_result.append(self._fit_linear(vin, adu_coarse))
-                #self._result["s_coarse_offset"]["data"] = fit_result[0]
-                #print(fit_result[0])
-                #print(adu_coarse)
-                #print(adu_coarse)
-                #idx_max = np.where(adu_coarse < 30)      
-                #idx_min = np.where(adu_coarse > 0 )
-                #if(idx_max[0][0]):
-                #    print(idx_max[0][0])
-                #print(idx_min[-1][-1])
-
-                #print("(", idx_max[0], ", ", idx_min[-1], ")")
-               
-                #print(idx_max[0], idx_min[0])
-                #idx_max = np.where(np.all(adu_coarse < 25))[0]
-                #idx_min = np.where(np.all(adu_coarse > 5))[0]
-
-                #if (idx_max-idx_min).all():
-                #if (idx_max[0][0] and idx_min[-1][-1]): 
-                #    print("here")
-                #    fit_result = self._fit_linear(vin[idx_max[0][0]:idx_min[-1][-1]], adu_coarse[idx_max[0][0]:idx_min[-1][-1]])
-                #else:
-                #    fit_result = self._fit_linear(vin, adu_coarse)
-                #print(idx_max, idx_min)
-                ##if idx:
-                ##    print(idx)
-                ##    print("oui oui")
-                ##    fit_result = self._fit_linear(vin[idx], adu_coarse[idx])
-                ##else:
-                ##    fit_result = self._fit_linear(vin, adu_coarse)
-                #fit_result_list.append(fit_result[0])
-=======
-        vin = self._fill_up_vin(data["vin"])  # noqa F841
-
-        # TODO
-        for adc in range(self._n_adcs):
             for col in range(self._n_cols):
-                # x = ... (subset of vin)
-                # y = ... (subset of data["s_coarse"][adc, col, :])
-                # res = self._fit_linear(x, y)
-                pass
->>>>>>> master
+                adu_coarse = sample_coarse[adc, col, :]
+                idx = np.where(np.logical_and(adu_coarse < 25, adu_coarse > 5))[0]
+                if np.any(idx):
+                    fit_result = self._fit_linear(vin[idx], adu_coarse[idx])
+                    slope[adc, col] = fit_result.solution[0]
+                    offset[adc, col] = fit_result.solution[1]
+                else:
+                    slope[adc, col] = np.NaN
+                    offset[adc, col] = np.NaN
 
-        #print(fit_result_list)
-        #self._result["s_coarse_offset"]["data"] = fit_result_list
-        #print(self._result["s_coarse_offset"]["data"])
-        #print(np.argwhere(aduCoarse == np.argmin(aduCoarse)))
-        #print("Start computing means and standard deviations ...", end="")
-        #offset = np.mean(data["s_coarse"], axis=2).astype(np.int)
-        #self._result["s_coarse_offset"]["data"] = offset
+        self._result["s_coarse_slope"]["data"] = slope
+        self._result["s_coarse_offset"]["data"] = offset
 
         self._result["stddev"]["data"] = data["s_coarse"].std(axis=2)
         print("Done.")
