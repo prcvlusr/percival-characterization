a8c59b125b6ce654bd4ed9819567857d0f8e7371
Manuela Kuhn <manuela.kuhn@desy.de>
2018-05-29 16:24:32 +0200

    Viewer: changed implicit configuration overwriting to explicit

--- characterization/src/raw/viewer_base.py
+++ characterization/src/raw/viewer_base.py
@@ -1,54 +1,26 @@
+import copy
 from collections import namedtuple
 import matplotlib.pyplot as plt
 
-from load_raw import LoadRaw
+from plot_base import PlotBase
 
 
-class ViewerBase():
-    LoadedData = namedtuple("loaded_data", ["data"])
+class ViewerBase(PlotBase):
+    def __init__(self, **kwargs):  # noqa F401
+        # overwrite the configured col and row indices
+        new_kwargs = copy.deepcopy(kwargs)
+        new_kwargs["frame"] = None
+        new_kwargs["col"] = None
+        new_kwargs["row"] = None
+        new_kwargs["dims_overwritten"] = True
 
-    def __init__(self, loaded_data=None, dims_overwritten=False, **kwargs):
+        super().__init__(**new_kwargs)
 
-        # add all entries of the kwargs dictionary into the class namespace
-        for key, value in kwargs.items():
-            setattr(self, "_" + key, value)
-
-        self._dims_overwritten = dims_overwritten
-
-        if self._frame is not None:
-            self._frame = None
-            self._dims_overwritten = True
-
-        loader = LoadRaw(input_fname=self._input_fname,
-                         metadata_fname=self._metadata_fname,
-                         output_dir=self._output_dir,
-                         frame=self._frame)
-
-        if loaded_data is None or self._dims_overwritten:
-            self._data = loader.load_data()
-        else:
-            self._data = loaded_data.data
+        print("Overwritten configuration " +
+              "(adc={}, frame={}, row={}, col={})"
+              .format(self._adc, self._frame, self._row, self._col))
 
         self._tracker = None
-
-    def get_dims_overwritten(self):
-        """If the dimension originally configures overwritten.
-
-        Return:
-            A boolean if the config war overwritten or not.
-        """
-        return self._dims_overwritten
-
-    def get_data(self):
-        """Exposes data outside the class.
-
-        Return:
-            A named tuble with the loaded data. Entries
-                x: filled up Vin read (to match the dimension of data)
-                data: sample and reset data
-        """
-
-        return ViewerBase.LoadedData(data=self._data)
 
     def plot_sample(self):
         pass
